dc08e605493956de9972bf5f83bc42913f82e5a5
Dudow <edu30.nm@gmail.com>
2020-05-25 21:50:42 -0300

    consegui atrapalhar o projeto

--- js/main.js
+++ js/main.js
@@ -1,24 +1,17 @@
 document.getElementById('formulario').addEventListener('submit', cadastraVeiculo)
+document.getElementById('confirmar').addEventListener('click', cadastraVeiculo)
 
 function cadastraVeiculo(e) {
 
-    var modeloCarro = document.getElementById('modeloCarro').value;
-    var placaCarro = document.getElementById('placaCarro').value;
-    var time = new Date();
+    var modeloCarro = document.getElementById('modeloCarro').value
+    var placaCarro = document.getElementById('placaCarro').value
+    var time = new Date()
 
     carro = {
         modelo: modeloCarro,
         placa: placaCarro,
         hora: time.getHours(),
-        minutos: time.getMinutes()
-    }
-
-    if (!modeloCarro) {
-        alert("Por favor, informe o modelo do carro");
-        return false;
-    } else if (!placaCarro) {
-        alert("Por favor, informe a placa do carro");
-        return false;
+        minuto: time.getMinutes()
     }
 
     if (placaRepetida(placaCarro)) {
@@ -27,58 +20,61 @@ function cadastraVeiculo(e) {
 
 
     if (localStorage.getItem('patio') === null) {
-        var carros = [];
-        carros.push(carro);
-        localStorage.setItem('patio', JSON.stringify(carros));
+        var carros = []
+        carros.push(carro)
+        localStorage.setItem('patio', JSON.stringify(carros))
     } else {
-        var carros = JSON.parse(localStorage.getItem('patio'));
-        carros.push(carro);
-        localStorage.setItem('patio', JSON.stringify(carros));
+        var carros = JSON.parse(localStorage.getItem('patio'))
+        carros.push(carro)
+        localStorage.setItem('patio', JSON.stringify(carros))
     }
 
     document.getElementById('formulario').addEventListener('submit', cadastraVeiculo).reset();
 
-    mostrarPatio();
+    mostrarPatio()
 
-    e.preventDefault();
+    e.preventDefault()
+
 }
 
 function mostrarPatio() {
-    var carros = JSON.parse(localStorage.getItem('patio'));
-    var carrosResultado = document.getElementById('resultados');
+    var patio = JSON.parse(localStorage.getItem('patio'))
+    var carros = document.getElementById('resultados')
+    var retirar = document.getElementById("confirmar")
 
-    carrosResultado.innerHTML = '';
+    retirar.onclick = () => retirarVeiculo(placa);
 
-    for (let i = 0; i < carros.length; i++) {
-        var modelo = carros[i].modelo;
-        var placa = carros[i].placa;
-        var hora = carros[i].hora;
-        var minutos = carros[i].minutos;
+    carros.innerHTML = ''
 
-        carrosResultado.innerHTML +=
-            '<tr><td>' + modelo +
+    for (let i = 0; i < patio.length; i++) {
+        var modelo = patio[i].modelo;
+        var placa = patio[i].placa;
+        var hora = patio[i].hora;
+        var minuto = patio[i].minuto;
+
+        carros.innerHTML += '<tr><td>' + modelo +
             '</td><td class="text-uppercase">' + placa +
-            '</td><td>' + hora +
-            ":" +
-            minutos +
-            '</td><td>' +
-            '<button class="btn btn-danger" onclick="retirarVeiculo(\'' + placa + '\')">Retirar Veículo</button>' +
-            '</td></tr>';
+            '</td><td>' + hora + ':' + minuto +
+            '</td><td>' + '<button data-toggle="modal" data-target="#modalDelete" class="btn btn-danger">Retirar</button>' +
+            '</td></tr>'
     }
 }
 
-function retirarVeiculo(placa) {
-    var carros = JSON.parse(localStorage.getItem('patio'));
 
-    for (let i = 0; i < carros.length; i++) {
-        if (carros[i].placa == placa) {
-            carros.splice(i, 1);
+
+function retirarVeiculo(placa) {
+
+    var patio = JSON.parse(localStorage.getItem('patio'))
+
+    for (var i = 0; i < patio.length; i++) {
+        if (patio[i].placa == placa) {
+            patio.splice(i, 1)
         }
 
-        localStorage.setItem('patio', JSON.stringify(carros));
+        localStorage.setItem('patio', JSON.stringify(patio))
     }
 
-    mostrarPatio();
+    mostrarPatio()
 }
 
 function placaRepetida(placaCarro) {
